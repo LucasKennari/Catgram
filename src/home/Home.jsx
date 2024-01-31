21b56be0f20082d81ba0107c77d339993d3c1b6b
Lucas Cezar <lucas.oheisen@gmail.com>
2024-01-31 18:00:20 -0300

    add component home

---
+++ src/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from '../components/helper/head/Head'
+import Feed from '../components/feed/feed/Feed'
+
+const Home = () => {
+
+          return (
+                    <section className='container mainContainer'>
+                              <Head title="Feed" description="Nome do site, com o feed de fotos" />
+                              <Feed />
+                    </section>
+          )
+}
+
+export default Home
